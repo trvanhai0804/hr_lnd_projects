ca4cf376fdfd2a45d4534a5e20eda5dd66b73b0c
trvanhai0804 <trvanhai0804@gmail.com>
2026-08-20 18:50:55 +0700

    Add files via upload

---
+++ projects/birthday/project-02/serve.ps1
@@ -0,0 +1,34 @@
+$root = $PSScriptRoot
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add("http://localhost:8421/")
+$listener.Start()
+Write-Host "Serving $root on http://localhost:8421/"
+
+$mime = @{
+  ".html"="text/html"; ".css"="text/css"; ".js"="application/javascript";
+  ".json"="application/json"; ".svg"="image/svg+xml"; ".png"="image/png";
+  ".jpg"="image/jpeg"; ".ico"="image/x-icon";
+}
+
+while ($listener.IsListening) {
+  $context = $listener.GetContext()
+  $req = $context.Request
+  $res = $context.Response
+  $path = $req.Url.LocalPath
+  if ($path -eq "/") { $path = "/index.html" }
+  $filePath = Join-Path $root ($path.TrimStart('/'))
+  if (Test-Path $filePath -PathType Leaf) {
+    $ext = [System.IO.Path]::GetExtension($filePath)
+    $ct = $mime[$ext]
+    if (-not $ct) { $ct = "application/octet-stream" }
+    $bytes = [System.IO.File]::ReadAllBytes($filePath)
+    $res.ContentType = $ct
+    $res.ContentLength64 = $bytes.Length
+    $res.OutputStream.Write($bytes, 0, $bytes.Length)
+  } else {
+    $res.StatusCode = 404
+    $msg = [System.Text.Encoding]::UTF8.GetBytes("Not found")
+    $res.OutputStream.Write($msg, 0, $msg.Length)
+  }
+  $res.OutputStream.Close()
+}
